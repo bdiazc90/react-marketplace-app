75454a3a3df468e55e325fb017d267f04bead45d
linder3hs <linderhassinger00@gmail.com>
2022-11-23 20:12:51 -0500

    feat: login view

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,35 @@
-import { Box } from "@mui/material";
+import { Box, TextField, Button, Paper, Typography } from "@mui/material";
 
 const Login = () => {
   return (
-    <Box>
-      <h1>Login</h1>
+    <Box
+      display="flex"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Paper
+        sx={{
+          width: 300,
+        }}
+      >
+        <Box p={3}>
+          <Typography textAlign="center" variant="h4">
+            Login
+          </Typography>
+          <Box my={3}>
+            <TextField label="E-mail" fullWidth />
+          </Box>
+          <Box my={3}>
+            <TextField label="Password" fullWidth />
+          </Box>
+          <Box my={3}>
+            <Button fullWidth variant="outlined">
+              Login
+            </Button>
+          </Box>
+        </Box>
+      </Paper>
     </Box>
   );
 };
